5e949fb072b62dca1a3a59ef0f4b6d8ed1c23f64
Luke Gill <bagrad007@gmail.com>
2021-05-25 12:22:59 -0400

    commit tree a33660cb218a4807a2242a8f6120afc4e2e78508

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,19 @@
-function changeState(state, action){
+function changeState(state, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+      return { count: state.count + 1 }
     default:
       return state;
   }
-}
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+  function dispatch(action) {
+    state = changeState(state, action)
+    render()
+  }
+}
+render()
+
+let state = { count: 0 }
+let action = { type: 'INCREASE_COUNT' }
 
 changeState(state, action)
